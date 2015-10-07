49057ca50402a7c46e51dd663a79ae2f3f5d4375
William Hester <williamehester@gmail.com>
2015-10-07 00:11:21 -0500

    Fix login. Login is now required to access most things on the site.

--- auacm/app/__init__.py
+++ auacm/app/__init__.py
@@ -1,19 +1,37 @@
 from flask import Flask, Response
+from flask.ext.login import LoginManager
+from flask.ext.bcrypt import Bcrypt
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from sqlalchemy.ext.automap import automap_base
 import os.path
 
+# flask setup
 app = Flask(__name__)
 app.config.from_pyfile('config.py') # not sure if this works
 
-Base = automap_base()
+# bcrypt setup
+bcrypt = Bcrypt(app)
 
+# database setup
+Base = automap_base()
 engine = create_engine('mysql://acm@localhost/acm')
 connection = engine.connect()
 Base.prepare(engine, reflect=True)
-
 session = Session(engine)
+
+# login session setup
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+from app.modules.user_manager.models import User
+@login_manager.user_loader
+def load_user(user_id):
+    result = session.query(Base.classes.users).filter(Base.classes.users.username=='will').first()
+    if result:
+        return User(result)
+    else:
+        return None
 
 # dirty hack from StackOverflow to allow us to serve static html
 def serve_html(filename):
--- auacm/app/views.py
+++ auacm/app/views.py
@@ -1,19 +1,30 @@
-from flask import render_template, jsonify
-from app import app, session, Base, serve_html
+from flask import render_template, jsonify, request
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from app import app, session, Base, serve_html, load_user, bcrypt
+from app.modules.user_manager.models import User
 # from app.modules.problem_manager.models import Problem
+
+def serve_response(response, response_code=200):
+    return jsonify({'status': response_code, 'data': response}), response_code
+
+def serve_error(error, response_code):
+    return jsonify({'status': response_code, 'error': error}), response_code
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     return render_template('login.html')
 
 @app.route('/problems')
+@login_required
 def getProblemsPage():
     return serve_html('problems.html')
 
 # ideally, this would be broken out into a different module, but we can
 # fix that later. For now, this works, and that's all that matters.
 @app.route('/api/problems')
+@login_required
 def getProblems():
     problems = list()
     for problem in session.query(Base.classes.problems).all():
@@ -26,7 +37,23 @@ def getProblems():
             'added': problem.added,
             'timeLimit': problem.time_limit
         })
-    return jsonify({
-        'status': 200,
-        'data': problems
-        }), 200
+    return serve_response(problems)
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    user = load_user(username)
+    if user:
+        hashed = user.user.passw
+        if bcrypt.check_password_hash(hashed, password):
+            # everything's gucci
+            login_user(user)
+            return serve_response({})
+    return serve_error('invalid username or password', 401)
+
+@app.route('/api/logout')
+@login_required
+def logout():
+    logout_user()
+    return serve_response({})
