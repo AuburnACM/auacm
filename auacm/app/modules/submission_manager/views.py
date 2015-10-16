3dfb348802f481cffc483af128f3c641caada958
William Hester <williamehester@gmail.com>
2015-10-15 23:14:57 -0500

    Make judge work for Java programs

--- auacm/app/modules/submission_manager/views.py
+++ auacm/app/modules/submission_manager/views.py
@@ -4,13 +4,22 @@ from app import app
 from app.database import Base, session
 from app.util import login_manager, serve_html, serve_response, serve_error
 from .models import Submission
-import os, time
+import os, time, subprocess
+from os.path import isfile, join
+from subprocess import Popen
+from time import sleep
 
-ALLOWED_EXTENSIONS = ['java', 'c', 'cpp']
+ALLOWED_EXTENSIONS = ['java', 'c', 'cpp', 'c++']
 
 def allowed_filetype(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+           
+def directory_for_submission(submission):
+    return join(app.config['DATA_FOLDER'], 'submits', str(submission.job))
+    
+def directory_for_problem(pid):
+    return join(app.config['DATA_FOLDER'], 'problems', pid)
 
 @app.route("/api/submit", methods=["POST"])
 @login_required
@@ -19,15 +28,91 @@ def submit():
     if not allowed_filetype(file.filename):
         return serve_error('filename not allowed', response_code=403)
     attempt = Submission(username=current_user.username,\
-            pid=int(request.form['pid']),\
+            pid=request.form['pid'],\
             submit_time=int(time.time()),\
             auto_id=0,\
-            file_type=file.filename.rsplit('.', 1)[1],\
+            file_type=file.filename.rsplit('.', 1)[1].lower(),\
             result='compile')
     session.add(attempt)
     session.flush()
     session.refresh(attempt)
-    directory = os.path.join(app.config['DATA_FOLDER'], str(attempt.job))
+    directory = directory_for_submission(attempt)
     os.mkdir(directory)
-    file.save(os.path.join(directory, file.filename))
+    file.save(join(directory, file.filename))
+    start_execution(attempt, file)
     return serve_response('{}')
+
+def start_execution(submission, file):
+    if submission.file_type=='java':
+        if compile_java(submission, file):
+            return
+        execute_java(submission, file)
+    elif submission.file_type=='c' or submission.file_type=='cpp' or submission.file_type=='c++':
+        result = compile_c(submission, file)
+        if not result:
+            return
+        result = execute_c(submission, file)
+
+def compile_java(submission, file):
+    directory = join(directory_for_submission(submission), file.filename)
+    if subprocess.call(['javac', directory]):
+        print 'compilation successful'
+        return True
+    else:
+        submission.result = 'compile'
+        session.flush()
+        print 'compilation failed'
+        return False
+        
+def get_process_handle(submission):
+    if submission.file_type='java':
+        pass
+    
+def execute_java(submission, file):
+    print 'beginning execution'
+    directory = directory_for_problem(submission.pid)
+    problem = session.query(Base.classes.problems)\
+            .filter(Base.classes.problems.pid==submission.pid).first()
+    input_path = join(directory, 'in')
+    output_path = join(directory, 'out')
+    input_files = [ f for f in os.listdir(input_path) if isfile(join(input_path, f)) ]
+    sub_out_directory = join(directory_for_submission(submission), 'out')
+    os.mkdir(sub_out_directory)
+
+    for in_file in input_files:
+        print 'checking file'
+        test_num = int(in_file.rsplit('.', 1)[0][2:])
+        process_handle = Popen(['java', '-cp',\
+                directory_for_submission(submission),\
+                file.filename.rsplit('.', 1)[0]],\
+                stdin=open(join(input_path, in_file)),\
+                stdout=open(join(sub_out_directory, "out" + str(test_num) + ".txt"), 'w'))
+                
+        timeout_time = time.time() + problem.time_limit
+        while process_handle.poll() is None and time.time() < timeout_time:
+            sleep(0.1)
+            
+        if process_handle.poll() is None:
+            # timeout
+            process_handle.terminate()
+        
+        with open(join(output_path, 'out' + str(test_num) + '.txt')) as test_output,\
+                open(join(directory_for_submission(submission), 'out', 'out' + str(test_num) + '.txt')) as generated:
+            test_lines = test_output.readlines()
+            generated_lines = generated.readlines()
+            
+            if not len(generated_lines) == len(test_lines):
+                print 'lengths different', len(generated_lines), len(test_lines)
+                submission.result = 'wrong'
+                session.flush()
+                return
+            
+            for i in range(0, len(test_lines)):
+                if not test_lines[i] == generated_lines[i]:
+                    submission.result = 'wrong'
+                    session.flush()
+                    return
+
+    submission.result = 'correct'
+    session.flush()
+    
