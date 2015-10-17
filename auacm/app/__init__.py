c5b46cc4080f8ccfde297aff7411434bf666040c
William Hester <williamehester@gmail.com>
2015-10-17 18:01:51 -0500

    Create scoreboard API.

--- auacm/app/__init__.py
+++ auacm/app/__init__.py
@@ -10,3 +10,4 @@ app.config.from_pyfile('config.py') # not sure if this works
 from app import util
 from app import views
 from app.modules.submission_manager import views
+from app.modules.scoreboard_manager import views
