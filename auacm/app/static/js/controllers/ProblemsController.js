cc77c443855d81d122a151e28622cf8df661febf
William Hester <williamehester@gmail.com>
2015-10-17 21:57:42 -0500

    Allow problems to be sorted by clicking on their titles.

--- auacm/app/static/js/controllers/ProblemsController.js
+++ auacm/app/static/js/controllers/ProblemsController.js
@@ -1,3 +1,8 @@
-app.controller('ProblemsController', ['$scope', '$http', function($scope, $http) {
-    
+app.controller('ProblemsController', ['$scope', '$filter', function($scope, $filter) {
+    var orderBy = $filter('orderBy');
+    $scope.order = function(predicate) {
+        $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
+        $scope.predicate = predicate;
+        $scope.problems = orderBy($scope.problems, predicate, $scope.reverse);
+    };
 }]);
