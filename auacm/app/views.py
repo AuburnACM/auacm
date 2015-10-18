0eace61749db65025126038eb3d927e78e1f8b54
BrandonLMorris <bmorris95@me.com>
2015-10-18 11:24:36 -0500

    Add load_user import to views.py
    The views module was missing the load_user import, so attempting
    to log in a user was throwing an error.

--- auacm/app/views.py
+++ auacm/app/views.py
@@ -2,7 +2,7 @@ from flask import render_template, request
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app
 from app.database import Base, session
-from app.util import bcrypt, login_manager, serve_html, serve_response, serve_error
+from app.util import bcrypt, login_manager, serve_html, serve_response, serve_error, load_user
 from app.modules.user_manager.models import User
 
 
