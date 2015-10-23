d2256952c76b5102cdc6eb27c31007c5c6fe101e
BrandonLMorris <bmorris95@me.com>
2015-10-23 13:47:05 -0500

    Add functionality to create new users
    Administrators can now go to their settings page and add a new
    user. The form calls the API (which was updated to require the
    caller to be an admin) and updates the database. Very little
    error checking was done and the form/requests fail silently
    (next to add).

--- auacm/app/static/js/controllers/SettingsController.js
+++ auacm/app/static/js/controllers/SettingsController.js
@@ -1,5 +1,6 @@
 app.controller('SettingsController', ['$scope', '$http', function($scope, $http) {
     $scope.changePassword = function() {
+        console.log($scope.newPassword);
         if ($scope.newPassword.length < 6) {
             return; // should probably tell the user no.
         }
@@ -16,7 +17,7 @@ app.controller('SettingsController', ['$scope', '$http', function($scope, $http)
             headers: {'Content-Type': 'application/x-www-form-urlencoded'},
             transformRequest: function(obj) {
                     var str = [];
-                    for(var p in obj)
+                    for (var p in obj)
                         str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
                     return str.join("&");
             },
@@ -32,5 +33,45 @@ app.controller('SettingsController', ['$scope', '$http', function($scope, $http)
         }, function(response) {
             console.log("error");
         });
+    }
+
+    $scope.createUser = function() {
+        console.log($scope.newUserDisplayName);
+        // Minimal error checking (should do a lot more)
+        if ($scope.newUserPassword.length < 6) {
+            return; // "should probably tell the user no"
+        }
+        if ($scope.newUserPassword !== $scope.newUserPasswordConfirm) {
+            return; // "tell the user that the passwords did not match"
+        }
+        var request = {
+            'username' : $scope.newUserName,
+            'password' : $scope.newUserPassword,
+            'display'  : $scope.newUserDisplayName
+        };
+
+        // Make the call to the api
+        $http({
+            method: 'POST',
+            url: '/api/create_user',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            transformRequest: function(obj) {
+                var str = [];
+                for (var p in obj)
+                    str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                console.log("Request: " + str.join("&"));
+                return str.join("&");
+            },
+            data: request
+        }).then(function(response) {
+            if (response.status == 200) {
+                // "everything's gucci"
+            } else {
+                // totes not gucci
+            }
+        }, function(response) {
+            console.log("Error in creating new user");
+        });
+        
     }
 }]);
