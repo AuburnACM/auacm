cc979426ec88a8bbe88b0e7bea5dac399605e19f
William Hester <williamehester@gmail.com>
2015-10-24 13:05:07 -0500

    Add database creation to acm.sql

--- setup/acm.sql
+++ setup/acm.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `blog_posts`
 --
 
+CREATE DATABASE acm;
+USE acm;
+
 DROP TABLE IF EXISTS `blog_posts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
