bf2cdc02aa1d0d559b09ac2ae22d4af1b7c2c7ad
Dave Shuckerow <djshuckerow@google.com>
2015-10-23 18:46:44 -0700

    Refactoring for additional modularity.

--- setup/initialize_database.sh
+++ setup/initialize_database.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-echo "Setting up mysql database."
-sudo mysql -uroot < acm.sql
 echo "Setting up submissions and problems data."
 mkdir ../auacm/app/data
 cp data.zip ../auacm/app/data/data.zip
--- setup/initialize_virtualenv.sh
+++ setup/initialize_virtualenv.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
-echo "Setting up virtualenv."
-sudo pip install virtualenv
 cd ../auacm/
 virtualenv flask
 flask/bin/pip install -r requirements.txt
 echo "Setting up nodejs and bower."
 npm install bower
 node_modules/bower/bin/bower install
-
