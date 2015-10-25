dd02236088c5eb67fcfe4514845e8c6bc41f9fbe
Dave Shuckerow <djshuckerow@google.com>
2015-10-25 04:31:08 -0700

    Final revert from setup scripts.

--- setup/initialize_virtualenv.sh
+++ setup/initialize_virtualenv.sh
@@ -6,4 +6,4 @@ virtualenv -p $(which python2.7) flask
 CFLAGS="-std=c99" ./flask/bin/pip install -r requirements.txt
 echo "Setting up nodejs and bower."
 npm install bower
-node_modules/bower/bin/bower install
+bower install
