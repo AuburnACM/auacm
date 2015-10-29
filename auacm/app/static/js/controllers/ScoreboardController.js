98279b83805bce83e6bc46e5c140b29fde9a91b6
BrandonLMorris <bmorris95@me.com>
2015-10-29 10:57:03 -0500

    First attempt at scoreboard timer
    This commit adds the basic functionality of adding a timer to the
    scoreboard. However, it has had embarassingly little testing. Also, the
    code assumes that the client and server clocks match up, which may not
    be the case. In the future, an offset should be added, or some other
    solution to prevent time mismatch. Also, a recalibration every so often
    would be a good idea as well.

--- auacm/app/static/js/controllers/ScoreboardController.js
+++ auacm/app/static/js/controllers/ScoreboardController.js
@@ -1,6 +1,11 @@
-app.controller('ScoreboardController', ['$scope', '$http', '$routeParams', '$window',
-    function($scope, $http, $routeParams, $window) {
+app.controller('ScoreboardController', ['$scope', '$http', '$routeParams', '$window', '$interval',
+    function($scope, $http, $routeParams, $window, $interval) {
+    // Store competition ID
     var cid = $routeParams.cid; 
+    // Initialize time left in competition to 0 so the user doesn't see 'null' on load
+    $scope.timeLeft = 0;
+
+
     var genScoreboard = function() {
         for (var i = 0; i < $scope.teams.length; i++) {
             var team = $scope.teams[i];
@@ -42,22 +47,46 @@ app.controller('ScoreboardController', ['$scope', '$http', '$routeParams', '$win
             prevSolved = team.solved;
             prevTime = team.time;
         }
-        $scope.$apply();
+
+        // Can break if an $apply is already in progress, need to check first (???)
+        if (!$scope.$$phase) {
+            $scope.$digest();
+        }
     }
+
+
+    // Generate the scoreboard on load
     $http.get('/api/competitions/' + cid)
         .then(function(response) {
             $scope.competition = response.data.data.competition;
             $scope.compProblems = response.data.data.compProblems;
             $scope.teams = response.data.data.teams;
             genScoreboard();
+
+            // TODO(brandonlmorris) Add offset to server/client times
+            $scope.TimeLeft = $scope.competition.length;
+            var thisTime = new Date().getTime();
+            var Timer = $interval(function() {
+                $scope.timeLeft = $scope.competition.startTime + $scope.competition.length - thisTime;
+                thisTime++;
+                if ($scope.timeLeft <= 0) {
+                    $scope.timeLeft = 0;
+                    $interval.cancel(Timer);
+                }
+            }, 1000);
         },
         function(error) {
             
         });
+
+
+    // Open the socket connection
     var socket = io.connect('http://' + $window.location.host + '/judge')
     socket.on('connect', function() {
         console.log('connected');
     });
+
+    // Perform live updates to the scoreboard
     var viewed = [];
     socket.on('status', function(event) {
         if (viewed.indexOf(event.submissionId) > -1 || event.status == 'running') {
