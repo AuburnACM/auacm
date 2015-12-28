200d8dd7b47cf9b329d79ad0df96767abc4ff38d
William Hester <williamehester@gmail.com>
2015-12-27 21:42:37 -0600

    Fix display of "Remaining" on /competitions.

--- auacm/app/static/js/controllers/CompetitionsController.js
+++ auacm/app/static/js/controllers/CompetitionsController.js
@@ -1,13 +1,18 @@
-app.controller('CompetitionsController', ['$scope', '$http', '$location', 
+app.controller('CompetitionsController', ['$scope', '$http', '$location',
         function($scope, $http, $location) {
     $scope.createCompetition = function() {
-        $location.path('/competitions/create')
-    }
+        $location.path('/competitions/create');
+    };
+
+    $scope.getRemainingTime = function(competition) {
+        return competition.startTime + competition.length - (Date.now() / 1000);
+    };
+
     $http.get('/api/competitions')
         .then(function(response) {
             $scope.competitions = response.data.data;
         },
         function(error) {
-            
+
         });
 }]);
