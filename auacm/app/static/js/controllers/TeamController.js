5e009183cf3426e528479ec84f70f8a83420084f
William Hester <williamehester@gmail.com>
2015-12-28 23:45:31 -0600

    Fix weird edge case; add dropdown to access team page

--- auacm/app/static/js/controllers/TeamController.js
+++ auacm/app/static/js/controllers/TeamController.js
@@ -25,6 +25,23 @@ app.controller('TeamController', ['$scope', '$http', '$location', '$window',
         $scope.teams[$scope.newTeamName] = [];
     };
     $scope.teamExists = function() {
+        // Need to make sure that the team name is available. If it is a user's
+        // display name that is currently in the competition as an individual,
+        // those two would end up on the same team even if they did not appear
+        // to be on the same team.
+        var i;
+        for (var team in $scope.teams) {
+            for (i = 0; i < $scope.teams[team].length; i++) {
+                if ($scope.newTeamName === $scope.teams[team][i].display) {
+                    return true;
+                }
+            }
+        }
+        for (i = 0; i < $scope.individuals.length; i++) {
+            if ($scope.newTeamName === $scope.individuals[i].display) {
+                return true;
+            }
+        }
         return $scope.newTeamName in $scope.teams;
     };
     $scope.removeTeam = function(name) {
@@ -66,9 +83,15 @@ app.controller('TeamController', ['$scope', '$http', '$location', '$window',
     var socket = io.connect('http://' + $window.location.host + '/register');
     socket.on('connect', function() {});
     socket.on('new_user', function(event) {
-        console.log('got new user', event);
         if ($routeParams.cid == event.cid) {
             $scope.individuals.push(event.user);
+
+            // rare edge-case, but just to be thorough, we need to check to see
+            // if a team with the same name as the user exists, and if so, break
+            // it down into individuals.
+            if (event.user.display in $scope.teams) {
+                $scope.removeTeam(event.user.display);
+            }
 
             if (!$scope.$$phase) {
                 $scope.$digest();
