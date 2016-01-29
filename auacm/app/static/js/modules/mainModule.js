a354f516f1d56f0ce1d2fec3fb99aaaed05d8b9a
William Hester <williamehester@gmail.com>
2016-01-28 21:59:06 -0600

    Enable tables support

--- auacm/app/static/js/modules/mainModule.js
+++ auacm/app/static/js/modules/mainModule.js
@@ -118,6 +118,7 @@ app.directive('fileModel', ['$parse', function ($parse) {
 
 app.directive('markdown', function () {
     var converter = new showdown.Converter();
+    converter.setOption('tables', 'true');
     return {
         restrict: 'A',
         link: function ($scope, element, attrs) {
