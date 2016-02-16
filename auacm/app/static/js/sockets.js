8d312d99c3a18e24ce1b31428785f23ba8622d57
William Hester <williamehester@gmail.com>
2016-02-15 21:37:35 -0600

    Remove 'this.' from Socket.js where possible

--- auacm/app/static/js/sockets.js
+++ auacm/app/static/js/sockets.js
@@ -1,11 +1,11 @@
 function Socket(url) {
-    this.ws = new WebSocket(url);
+    ws = new WebSocket(url);
     this.events = {};
     var parent = this;
     this.on = function(event, func) {
         parent.events[event] = func;
     };
-    this.ws.onmessage = function(event) {
+    ws.onmessage = function(event) {
         var data = JSON.parse(event.data);
         if (data.eventType in parent.events) {
             parent.events[data.eventType](data.data);
