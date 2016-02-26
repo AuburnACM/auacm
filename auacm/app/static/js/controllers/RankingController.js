57bdb9e4dd65b1773404d89ff043be8f4429ce0a
brandonlmorris <brandon.morris95@gmail.com>
2016-02-25 20:02:03 -0600

    Ranking time frame client support
    This commit now provides the user with selection options
    for picking the time frame  in which to rank users, and the
    table automatically updates to reflect the selection.

--- auacm/app/static/js/controllers/RankingController.js
+++ auacm/app/static/js/controllers/RankingController.js
@@ -2,7 +2,9 @@ app.controller('RankingController', ['$scope', '$route', '$http',
     function($scope, $route, $http) {
     $scope.display = false;
     $scope.ranks = [];
+    $scope.timeFrame = 'all';
 
+    // API call to get the ranking of the users
     var getRanking = function() {
         $http.get('/api/ranking')
             .then(function(response) {
@@ -14,4 +16,15 @@ app.controller('RankingController', ['$scope', '$route', '$http',
             })
     };
     getRanking();
+
+    // Get a filtered ranking when the user selects a time frame
+    $scope.onTimeFrameChange = function() {
+        $http.get('/api/ranking/' + $scope.timeFrame)
+            .then(function(response) {
+                $scope.ranks = response.data.data;
+            },
+            function (error) {
+                console.error(error);
+            })
+    };
 }]);
