6ef6555f56b5d664b04cd7fbfb39aaa1d9bc8ab2
William Hester <williamehester@gmail.com>
2016-08-17 22:02:19 -0700

    Fix problem timing, update README.md

--- auacm/app/modules/problem_manager/problem_timer.py
+++ auacm/app/modules/problem_manager/problem_timer.py
@@ -1,45 +1,66 @@
 import math
 import os
 import shutil
+import sys
 
-from app import app
+from app import config
 from app.modules.submission_manager import judge
 from app.modules.submission_manager.models import MockSubmission
 from app.modules.submission_manager.judge_test import MockUploadFile
 
 
-PROBLEMS_DIR = os.path.join(app.config['DATA_FOLDER'], 'problems')
+PROBLEMS_DIR = os.path.join(config.DATA_FOLDER, 'problems')
 
 
 class Timer:
+    """Timer times all of the judge solutions for a problem."""
 
     def __init__(self, problem):
         self.problem = problem
         self.path = os.path.join(PROBLEMS_DIR, str(problem.pid), 'test')
-        self.submission_path = os.path.join(os.getcwd(), 'temp', 'submits')
+        self.submission_path = os.path.join(config.TEMP_DIRECTORY, 'submits')
 
-    def _set_up(self):
-        try:
-            os.makedirs(os.path.join(self.submission_path, str(self.problem.pid)))
-        except OSError:
-            pass
+    def _create_temp_directory(self):
+        """Attempts to create a temp directory.
 
-    def _tear_down(self):
-        try:
-            shutil.rmtree(self.submission_path)
-        except OSError:
-            pass
+        In the event that we can't create the temp directory, an OSError will
+        be raised. In that case, you probably want to specify a different
+        directory to create the temp files in in config.py.
+        """
+        os.makedirs(os.path.join(self.submission_path, str(self.problem.pid)))
 
-    def _time(self):
-        """Times all of the problems in the problem's test folder
+    def _remove_temp_files(self):
+        """Removes the temporary files.
 
-        The problem
+        In the event that we can't remove the file tree that was created, an
+        OSError will be raised. In that case, you should probably choose a new
+        temp directory as well as delete the old temp directory yourself.
+        """
+        shutil.rmtree(self.submission_path)
 
+    def _run(self):
+        """Times all of the problems in the problem's test folder.
+
+        This will iterate through all of the files in the problem's test
+        folder. For each file, it creates a mock submission (so that it does
+        not have to store a submission in the database), and a "Mock" upload
+        file. The "Mock" upload file still depends on the computer's
+        filesystem, because the Judge is dependent upon the computer's
+        filesystem.
+
+        Each file is run in serial, and the maximum time of all of the judge
+        submissions, times 1.5 (just to give people some wiggle-room). Then,
+        it is "standardized" by dividing by the judge's TIMEOUT_MULTIPLIER for
+        the given file type.
+
+        :raises NoJudgeSolutionError: in the event that no judge solution is
+            found for the given problem.
         """
         files = os.listdir(self.path)
         if len(files) == 0:
             raise NoJudgeSolitionsError(self.problem.pid)
 
+        found_max = 0
         for fname in files:
             source = os.path.join(self.path, fname)
             if not os.path.isfile(source):
@@ -49,29 +70,33 @@ class Timer:
 
             file_type = fname.rsplit('.', 1)[1].lower()
             sub = MockSubmission(
-                pid=self.problem.pid, file_type=file_type, job=self.problem.pid)
+                pid=self.problem.pid,
+                file_type=file_type,
+                job=self.problem.pid
+            )
+            submission_path = os.path.join(self.submission_path, str(sub.job))
             upload = MockUploadFile(source)
-            upload.save(os.path.join(self.submission_path, str(sub.job), fname))
-            judgement = judge.Judge(sub, self.submission_path, upload, 120 if file_type != 'cpp' else 10)
+            upload.save(os.path.join(submission_path, fname))
+
+            judgement = judge.Judge(sub.pid, submission_path, upload, 120 if file_type != 'cpp' else 10)
             status, time = judgement.run()
 
             if status == judge.CORRECT_ANSWER:
                 time_limit = int(math.ceil(
                         time * 1.5 / judge.TIMEOUT_MULTIPLIER[file_type]))
-                self.problem.time_limit = time_limit
+                found_max = max(found_max, time_limit)
+                self.problem.time_limit = found_max
                 self.problem.commit_to_session()
-                return True
-            else:
-                return False
 
     def run(self):
-        """
-
-        """
-        self._set_up()
-        result = self._time()
-        self._tear_down()
-        return result
+        """Executes the problem timer."""
+        self._create_temp_directory()
+        try:
+            self._run()
+        except NoJudgeSolitionsError as e:
+            print(str(e), file=sys.stderr)
+        else:
+            self._remove_temp_files()
 
 
 class NoJudgeSolitionsError(Exception):
--- auacm/time_problems.py
+++ auacm/time_problems.py
@@ -1,109 +1,14 @@
 #!flask/bin/python
-import glob
-import math
-import os
-
-from app import app, database
-from app.modules.submission_manager import judge
-from app.modules.problem_manager.models import ProblemData
-from app.modules.submission_manager.models import MockSubmission
-from app.modules.submission_manager.judge_test import MockUploadFile
-
-
-submit_file = None
-MOCK_SUBMIT_DIR = os.path.join(
-        app.config['DATA_FOLDER'], 'submits', 'mocksubmit')
-PROBLEMS_DIR = os.path.join(app.config['DATA_FOLDER'], 'problems')
-
-
-def setUp():
-    """Prepare the test directory to run a new submit.
-
-    1. Reconfigure app to go to the directory app/test_data
-    2. Empty the submits directory
-    """
-    try:
-        os.makedirs(
-                os.path.join(app.config['DATA_FOLDER'], 'submits',
-                'mocksubmit'))
-    except OSError:
-        pass
-
-
-def _purgeDirectory(directory):
-    """Recursively empty out the contents of a directory. USE WITH CAUTION.
-
-    This is currently used to empty out the judge_tests/submits directory
-    before and after tests are run.
-    """
-    for f in glob.glob(directory):
-        if os.path.isfile(f):
-            os.remove(f)
-        if os.path.isdir(f):
-            _purgeDirectory(os.path.join(f, '*'))
-            os.rmdir(f)
-
-
-def tearDown():
-    """Post-test cleanup method.
-
-    Close our submit_file so that we don't waste resources waiting on
-    the garbage collector.
-
-    Purge the judge_tests/submits directory post-test.
-    """
-    if submit_file:
-        submit_file.close()
-    try:
-        _purgeDirectory(os.path.join(MOCK_SUBMIT_DIR, '*'))
-        os.rmdir(MOCK_SUBMIT_DIR)
-    except OSError:
-        pass
+from app import database
+from app.modules.problem_manager import models
+from app.modules.problem_manager import problem_timer
 
 
 def time_problems():
-    problems = database.session.query(ProblemData).all()
+    problems = database.session.query(models.ProblemData).all()
     for problem in problems:
-        path = os.path.join(PROBLEMS_DIR, str(problem.pid), 'test')
-        files = os.listdir(path)
-        if len(files) == 0:
-            print('No sample solutions found for pid:', problem.pid)
-        for fname in files:
-            fyle = os.path.join(path, fname)
-            if os.path.isfile(fyle):
-                if not judge.allowed_filetype(fname):
-                    # os.remove(fyle)
-                    print('unsupported file: ', fyle)
-                    os.remove(fyle)
-                    continue
-                # if fname.rsplit('.')[1] == 'cpp':
-                #     print('skipping c++')
-                #     continue
+        problem_timer.Timer(problem).run()
 
-                file_type = fname.rsplit('.', 1)[1].lower()
-                sub = MockSubmission(
-                    username='Test User',
-                    pid=problem.pid,
-                    submit_time=0,
-                    auto_id=0,
-                    file_type=file_type,
-                    result='start'
-                )
-                upload = MockUploadFile(fyle)
-                upload.save(os.path.join(MOCK_SUBMIT_DIR, fname))
-                judgement = judge.Judge(sub.pid, MOCK_SUBMIT_DIR, upload,
-                        120 if file_type != 'cpp' else 10)
-                status, time = judgement.run()
-
-                if status == judge.CORRECT_ANSWER:
-                    time_limit = time
-                    problem.time_limit = time_limit
-                    problem.commit_to_session()
-                    print(problem.pid, time_limit)
-                else:
-                    print(problem.pid, 'judge solution incorrect')
 
 if __name__ == '__main__':
-    setUp()
     time_problems()
-    tearDown()
