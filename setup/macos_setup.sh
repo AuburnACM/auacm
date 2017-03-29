145dec6ee0557caee7c36f947d991f7a7dd085b5
Mac-Genius <jwh0051@auburn.edu>
2017-03-28 22:50:38 -0500

    Add config configuration

--- setup/macos_setup.sh
+++ setup/macos_setup.sh
@@ -1,4 +1,5 @@
-sh macos/macos_dependencies.sh
-sh initialize_database.sh
-sh initialize_virtualenv.sh
+sh setup_macos/macos_dependencies.sh
+sh setup_all/initialize_database.sh
+sh setup_all/initialize_virtualenv.sh
+sh setup_all/initialize_config.sh
 # Finished building the website. You can now run ./run.py
--- setup/ubuntu_setup.sh
+++ setup/ubuntu_setup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 echo "This script will set up AUACM."
 sudo sh ubuntu/ubuntu_dependencies.sh
-sh initialize_database.sh
-sh initialize_virtualenv.sh
+sh setup_all/initialize_database.sh
+sh setup_all/initialize_virtualenv.sh
+sh setup_all/initialize_config.sh
 echo "Setup complete!  You should now be able to launch the server."
 echo "To get started execute $ cd ../auacm && ./run.py to get started"
