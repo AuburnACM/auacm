3111dc601701a2b6334e361775fafea7bee06c45
Mac-Genius <jwh0051@auburn.edu>
2017-03-29 10:19:25 -0500

    Add default profile pic to data.zip file

--- setup/macos_setup.sh
+++ setup/macos_setup.sh
@@ -1,5 +1,5 @@
 sh setup_macos/macos_dependencies.sh
 sh setup_all/initialize_database.sh
-sh setup_all/initialize_virtualenv.sh
 sh setup_all/initialize_config.sh
+sh setup_all/initialize_virtualenv.sh
 # Finished building the website. You can now run ./run.py
